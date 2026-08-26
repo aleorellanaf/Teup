ba8160c25846db6c42292bbe40829deac0a44617
Tu Nombre o Usuario de GitHub <alejandro.fof@gmail.com>
2026-08-25 21:28:09 -0400

    s

--- database/db.py
+++ database/db.py
@@ -3,8 +3,9 @@ import pyodbc
 def obtener_conexion():
     conn_str = (
         "DRIVER={ODBC Driver 17 for SQL Server};"
-        "SERVER=localhost;" 
+        "SERVER=127.0.0.1,1433;"
         "DATABASE=TaupDB;"
-        "Trusted_Connection=yes;"
+        "UID=sa;"
+        "PWD=TuPassword123;"
     )
     return pyodbc.connect(conn_str)
